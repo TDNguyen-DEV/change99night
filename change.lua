2857a8655aea9a00c6933f2575404bdbee9613bb
TDNguyen-DEV <trandangnguyen001@gmail.com>
2025-09-24 21:52:49 +0700

    Update change.lua

--- change.lua
+++ change.lua
@@ -5,10 +5,10 @@ repeat
 until game:IsLoaded() and game.Players.LocalPlayer:GetAttribute("DataHasLoaded")
 repeat task.wait() until game:IsLoaded() and game.Players.LocalPlayer
 
-while task.wait()
+while task.wait() do
     local gem = game.Players.LocalPlayer:GetAttribute("Diamonds")
     if gem and typeof(gem) == "number" and gem >= getgenv().Gem then
-        writefile(game.Players.LocalPlayer.Name .. ".txt", "Completed-" .. tostring(gem) .. )
+        writefile(game.Players.LocalPlayer.Name .. ".txt", "Completed-" .. tostring(getgenv().Gem) .. " gem 99 night")
         break
     end
 end
